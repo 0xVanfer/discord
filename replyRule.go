1ab0d4cf4f89b6d265b5228a48089415d1dbb1d5
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-11-16 15:35:45 +0800

    [fix] i'm dead

--- replyRule.go
+++ replyRule.go
@@ -3,6 +3,7 @@ package discord
 import (
 	"strings"
 
+	"github.com/0xVanfer/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -21,39 +22,38 @@ func (bot *DiscordBot) AddReplyRule(rule ReplyRule) {
 			// Last read map add this new channel.
 			bot.LastRead[channelID] = MsgInfo{}
 		}
-		// Add a new rule.
-		bot.ReplyRules = append(bot.ReplyRules, rule)
 	}
+	// Add a new rule.
+	bot.ReplyRules = append(bot.ReplyRules, rule)
 }
 
 // Whether the rule should be replied.
 func (rule *ReplyRule) shouldReply(msg *discordgo.Message) bool {
 	// Should not reply if channel not match.
-	for _, channel := range rule.ChannelIDs {
-		if strings.EqualFold(msg.ChannelID, channel) {
-			switch rule.RuleType {
-			// Equalfold.
-			case 0:
-				return strings.EqualFold(rule.CheckText, msg.Content)
-			// Contain.
-			case 1:
-				return strings.Contains(strings.ToLower(msg.Content), strings.ToLower(rule.CheckText))
-			// Start with.
-			case 2:
-				if len(msg.Content) <= len(rule.CheckText) {
-					return false
-				}
-				if !strings.EqualFold(msg.Content[:len(rule.CheckText)], rule.CheckText) {
-					return false
-				}
-				if rule.LengthLimit == 0 {
-					return true
-				}
-				return len(msg.Content[len(rule.CheckText):]) == rule.LengthLimit
-			default:
-				return false
-			}
-		}
+	if !utils.ContainInArrayX(msg.ChannelID, rule.ChannelIDs) {
+		return false
 	}
-	return false
+	switch rule.RuleType {
+	// Equalfold.
+	case 0:
+		return strings.EqualFold(rule.CheckText, msg.Content)
+	// Contain.
+	case 1:
+		return strings.Contains(strings.ToLower(msg.Content), strings.ToLower(rule.CheckText))
+	// Start with.
+	case 2:
+		if len(msg.Content) <= len(rule.CheckText) {
+			return false
+		}
+		if !strings.EqualFold(msg.Content[:len(rule.CheckText)], rule.CheckText) {
+			return false
+		}
+		if rule.LengthLimit == 0 {
+			return true
+		}
+		return len(msg.Content[len(rule.CheckText):]) == rule.LengthLimit
+	default:
+		return false
+	}
+
 }
