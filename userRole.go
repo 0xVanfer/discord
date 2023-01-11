34cfbeabca77730801288ccfb3fd49d25f0216d8
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2023-01-11 11:36:51 +0800

    [feat] add members

--- userRole.go
+++ userRole.go
@@ -1,6 +1,9 @@
 package discord
 
-import "github.com/0xVanfer/utils"
+import (
+	"github.com/0xVanfer/utils"
+	"github.com/bwmarrin/discordgo"
+)
 
 // Check if the user is the role of the guild.
 func (bot *DiscordBot) CheckUserRole(userID string, guildID string, roleID string) (isRole bool) {
@@ -9,4 +12,18 @@ func (bot *DiscordBot) CheckUserRole(userID string, guildID string, roleID strin
 		return false
 	}
 	return utils.ContainInArray(roleID, member.Roles)
+}
+
+// Read all members of a role.
+func (bot *DiscordBot) FilterRole(guildID string, roleID string) (roleMembers []*discordgo.Member, err error) {
+	allMembers, err := bot.AllMembers(guildID)
+	if err != nil {
+		return
+	}
+	for _, member := range allMembers {
+		if utils.ContainInArray(roleID, member.Roles) {
+			roleMembers = append(roleMembers, member)
+		}
+	}
+	return
 }
---
+++ users.go
@@ -0,0 +1,19 @@
+package discord
+
+import "github.com/bwmarrin/discordgo"
+
+// Read all members of a guild.
+func (bot *DiscordBot) AllMembers(guildID string) (allMembers []*discordgo.Member, err error) {
+	lastID := ""
+	for {
+		newMembers, err := bot.Session.GuildMembers(guildID, lastID, 1000)
+		if err != nil {
+			return nil, err
+		}
+		allMembers = append(allMembers, newMembers...)
+		if len(newMembers) < 1000 {
+			return allMembers, nil
+		}
+		lastID = newMembers[len(newMembers)-1].User.ID
+	}
+}
